3436580ea4c014dcf63dd962e1a866de7c20c206
dkorenivskiy <d.korenivskiy@gmail.com>
2021-12-21 21:26:26 +0200

    practice 1 - done

---
+++ Module3/Practice1/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            Console.WriteLine($"x + y = {x + y}");
+            return (x + y);
+        }
+
+        public int Difference(int x, int y)
+        {
+            Console.WriteLine($"x - y = {x - y}");
+            return (x - y);
+        }
+
+        public int Product(int x, int y)
+        {
+            Console.WriteLine($"x * y = {x * y}");
+            return (x * y);
+        }
+
+        public int Division(int x, int y)
+        {
+            try
+            {
+                Console.WriteLine($"x / y = {x / y}");
+                return (x / y);
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Module3/Practice1/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice1
+{
+    class Program
+    {
+        public delegate int CalcOperation(int x, int y);
+
+        static CalcOperation calcOperation;
+
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+
+            string choice;
+            int count = 0;
+            do
+            {
+                if (count == 0)
+                {
+                    Console.WriteLine("=====Calculator=====");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Add another method or start program");
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("1 - (x + y)");
+                Console.WriteLine("2 - (x - y)");
+                Console.WriteLine("3 - (x * y)");
+                Console.WriteLine("4 - (x / y)");
+                Console.WriteLine("5 - Result of program");
+                Console.WriteLine("0 - exit");
+                choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        calcOperation += calculator.Add;
+                        count++;
+                        break;
+
+                    case "2":
+                        calcOperation += calculator.Difference;
+                        count++;
+                        break;
+
+                    case "3":
+                        calcOperation += calculator.Product;
+                        count++;
+                        break;
+
+                    case "4":
+                        calcOperation += calculator.Division;
+                        count++;
+                        break;
+
+                    case "5":
+                        Start(calcOperation);
+                        count = 0;
+                        Console.WriteLine("Type any key to continue");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case "0":
+                        break;
+
+                    default:
+                        Console.WriteLine("Wrong value!");
+                        break;
+                }
+            } while (choice != "0");
+        }
+
+        private static void Start(CalcOperation calcOperation)
+        {
+            Console.WriteLine("Enter x");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y");
+            int y = Convert.ToInt32(Console.ReadLine());
+            var listOfCalcOperations = calcOperation.GetInvocationList();
+            int sum = 0;
+
+            foreach (var operation in listOfCalcOperations)
+            {
+                try
+                {
+                    sum += (int)operation.DynamicInvoke(x, y);
+                }
+                catch(DivideByZeroException ex)
+                {
+                    Console.WriteLine($"Error! {ex.Message}");
+                } 
+            }
+
+            Console.WriteLine($"Sum = {sum}");
+        }
+    }
+}
+
+
+
