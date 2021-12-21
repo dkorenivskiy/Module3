1d259b708818f36a54c613b75ec5a99e9c540cc8
Korenivskyi, Danylo <d.korenivskiy@gmail.com>
2021-12-21 17:21:00 +0200

    lesson 03 - in progress

---
+++ lesson03/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson03
+{
+    class Class1
+    {
+        public delegate int 
+
+        static int Pow(int a, int b)
+        {
+            return (a * b);
+        }
+    }
+}
