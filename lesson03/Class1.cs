d08e8c3489c78ce358c28581847c2bf06d6f0944
dkorenivskiy <d.korenivskiy@gmail.com>
2021-12-28 19:49:45 +0200

    not ready yet/

--- lesson03/Class1.cs
+++ lesson03/Class1.cs
@@ -6,9 +6,18 @@ using System.Threading.Tasks;
 
 namespace lesson03
 {
+    public delegate int PowDelegate(int a, int b);
+
     class Class1
     {
-        public delegate int 
+        public delegate void boolDelegate();
+
+        public PowDelegate powDelegate;
+
+        public Class1()
+        {
+            powDelegate = Pow;
+        }
 
         static int Pow(int a, int b)
         {
--- lesson03/Class2.cs
+++ lesson03/Class2.cs
@@ -8,5 +8,32 @@ namespace lesson03
 {
     class Class2
     {
+        public delegate bool ResultDelegate(int result);
+
+        public ResultDelegate resultDelegate;
+
+        public Class2()
+        {
+            resultDelegate = Result;
+        }
+
+        private int _result;
+
+        private Class1 _class1 = new Class1();
+
+        private ResultDelegate Calc(int a, int b, PowDelegate powDelegate)
+        {
+            powDelegate = _class1.powDelegate;
+            _result = powDelegate(a, b);
+
+
+        }
+
+        private bool Result(int result)
+        {
+
+
+            return false;
+        }
     }
 }
