54b82f519d6f1c2e497a9b743b65249e37e44862
dkorenivskiy <d.korenivskiy@gmail.com>
2021-12-28 21:41:27 +0200

    practice2 - done

---
+++ Module3/practice2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace practice2
+{
+    enum State
+    {
+        Ok,
+        Cancel
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            MessageBox messageBox = new MessageBox();
+            messageBox.windowOpenClose += Open;
+            messageBox.Open(cts.Token);
+
+            string quitProgram = Console.ReadKey().ToString();
+            if (quitProgram == "Q" || quitProgram == "q")
+            {
+                cts.Cancel();
+            }
+        }
+
+        private static void Open(State state)
+        {
+            if (state == State.Ok)
+            {
+                Console.WriteLine("Ok - operation succeed\n");
+            }
+
+            if (state == State.Cancel)
+            {
+                Console.WriteLine("Cancel - operation unsucceed\n   ");
+            }
+        }
+    }
+}
