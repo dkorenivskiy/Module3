f60f4cf4a77a1cee2abdabc9297d3a527b752150
Korenivskyi, Danylo <d.korenivskiy@gmail.com>
2022-01-11 16:19:23 +0200

    lesson05 - done

---
+++ Module3/lesson05/Logger.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lesson05
+{
+    public class Logger
+    {
+        private const string fileName = "log.log";
+
+        private readonly int _maxLogItemCount; 
+        private int _logItemCount;
+        private readonly object _lock = new object();
+        private readonly AutoResetEvent waitHandle = new AutoResetEvent(true);
+
+        private Starter _starter = new Starter();
+
+        public Logger(int maxCount)
+        {
+            _maxLogItemCount = maxCount;
+        }
+
+        public event EventHandler<CustomEventArgs> OnLogMax;
+
+        public async Task Log(string message)
+        {
+            waitHandle.WaitOne();
+
+            await File.AppendAllTextAsync(fileName, $"[{DateTime.Now}] {message}\n");
+            Interlocked.Increment(ref _logItemCount);
+            if (_logItemCount == _maxLogItemCount)
+            {
+                EventHandler<CustomEventArgs> eventHandler = OnLogMax;
+
+                if (eventHandler != null)
+                {
+                    eventHandler(this, new CustomEventArgs(fileName));
+                }
+
+                _logItemCount = 0;
+            }
+
+            waitHandle.Set();
+        }
+    }
+}
---
+++ Module3/lesson05/Program.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lesson05
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if(File.Exists("log.log"))
+            {
+                File.Delete("log.log");
+            }
+
+            var configFile = File.ReadAllText("appsettings.json");
+            var config = JsonConvert.DeserializeObject<LoggerConfig>(configFile);
+
+            var logger = new Logger(config.MaxLogItemCount);
+            var starter = new Starter();
+            logger.OnLogMax += starter.LogBackup;
+
+            List<Task> tasks = new List<Task>();
+            for(var i = 0; i < 2; ++i)
+            {
+                tasks.Add(Task.Run(async () =>
+                {
+                    for (var i = 0; i < 50; ++i)
+                    {
+                        await logger.Log($"task # {Thread.CurrentThread.ManagedThreadId}");
+                        await Task.Delay(20);
+                    }
+                }));
+            }            
+
+            await Task.WhenAll(tasks.ToArray());
+        }
+    }
+}
